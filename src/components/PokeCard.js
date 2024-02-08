61b8af698727b85dd24e89f3dc98c34d82bef20f
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-02-08 15:06:37 -0600

    remove commented code

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -62,19 +62,6 @@ console.log(center.y / 100)
       </Card.Body>
       <div className="glow" ref={glowRef}/>
     </Card>
-    // <ul className="card-container col-3">
-    //   <div className="card text-center mx-auto" style={{"maxWidth" : "18rem"}}>
-    //     <div className="card-header">
-    //       <b>{name}</b>
-    //     </div>
-    //     <div className="card-body">          
-    //       <h6 className="card-subtitle mb-2 text-muted">Id: {pokeData.id}</h6>  
-    //       <h6 className="card-subtitle mb-2 text-muted">Height: {pokeData.height}</h6>  
-    //       <h6 className="card-subtitle mb-2 text-muted">Weight: {pokeData.weight}</h6>   
-    //       <h6 className="card-subtitle mb-2 text-muted">Experience: {pokeData.base_experience}</h6>  
-    //     </div>
-    //   </div>
-    // </ul> 
   );
 };
 
