67c1e36391e2085610c49755243d993b848e346c
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-02-17 12:47:46 -0600

    change loader package

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Audio } from 'react-loader-spinner'
 
 const LoadingAnimation = (props) => {
-    if(props.loading) {
+    const loading = true;
+    if(loading) {
         return(
             <div className='loader row'>
                 <p className='col-2'>Loading Pokemons</p>
-                <div className='col-1'>"
+                <div className='col-1'>
+                    <Audio
+                        height="30"
+                        width="10"
+                        radius="10"
+                        color="green"
+                    />
                 </div>
             </div>
         );
     } else {
-        return <div></div>;
+        return <div></div>
     }
 };
 
