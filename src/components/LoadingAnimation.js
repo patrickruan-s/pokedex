dcd89b5952e4247fcc0096307995be819ed63336
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-02-17 12:42:22 -0600

    Remove loader

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { CircleLoader} from "react-awesome-loaders"
-
 
 const LoadingAnimation = (props) => {
     if(props.loading) {
         return(
             <div className='loader row'>
                 <p className='col-2'>Loading Pokemons</p>
-                <div className='col-1'>
-                    <CircleLoader
-                        meshColor={"#6366F1"}
-                        lightColor={"#E0E7FF"}
-                        duration={1.5}
-                        desktopSize={"10px"}
-                        mobileSize={"10px"}
-                    />
+                <div className='col-1'>"
                 </div>
             </div>
         );
