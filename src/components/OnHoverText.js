0b0efb87784e61fd9d460eb73c5386236135cf1c
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-02-20 13:24:03 -0600

    add ability hover

--- src/components/OnHoverText.js
+++ src/components/OnHoverText.js
@@ -7,19 +7,16 @@ const OnHoverText = (props) => {
     const hoverContent = props.hoverContent;
 
     return(
-        <div>
             <div className='hoverable-text'
-                 onMouseEnter={e => {
-                     setStyle({opacity: 1, height: 'fit-content'});
-                 }}
-                 onMouseLeave={e => {
-                     setStyle({opacity: 0, height: '0px'})
-                 }}
-            >
-                <div>{text}</div>
+            onMouseEnter={e => {
+                setStyle({opacity: 1, height: '50px'});
+            }}
+            onMouseLeave={e => {
+                setStyle({opacity: 0, height: '0px'})
+            }}>
+                {text}
+                <Card className='hoverable-content' style={style}>{hoverContent}</Card>
             </div>
-        <Card className='hoverable-content' style={style}>{hoverContent}</Card>
-        </div>
     )
 }
 
