3698a5a2bb538a6a731ebc972dffe7bfb78f51f2
Patrick Ruan <patrickruan@Patricks-MacBook-Air-2.local>
2024-02-19 22:18:59 -0600

    remove debugger

--- src/components/ExpandedPokeCard.js
+++ src/components/ExpandedPokeCard.js
@@ -14,7 +14,6 @@ const ExpandedPokeCard = (props) => {
           return str.charAt(0).toUpperCase() + str.slice(1);
         }
 
-        debugger
         return (
             <Card className='expanded-card'>
                 <Card.Header>
